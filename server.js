e7818fba814e2fcf707c20d9fc994efe8a6c5810
osxzxso <osxzxso@gmail.com>
2023-05-20 16:21:29 -0500

    Removed "DC", "GU", "PR", "VI" as well (not states)

--- server.js
+++ server.js
@@ -141,7 +141,7 @@ app.get('/state/:statename', async (req, res) => {
     const stateName = req.params.statename.toUpperCase();
 
     const statePage = path.join(template_dir, 'state.html');
-    const states = ["AK", "AL", "AR", "AZ", "CA", "CO", "CT", "DC", "DE", "FL", "GA", "GU", "HI", "IA", "ID", "IL", "IN", "KS", "KY", "LA", "MA", "MD", "ME", "MI", "MN", "MO", "MS", "MT", "NC", "ND", "NE", "NH", "NJ", "NM", "NV", "NY", "OH", "OK", "OR", "PA", "PR", "RI", "SC", "SD", "TN", "TX", "UT", "VA", "VI", "VT", "WA", "WI", "WV", "WY"];
+    const states = ["AK", "AL", "AR", "AZ", "CA", "CO", "CT", "DE", "FL", "GA", "HI", "IA", "ID", "IL", "IN", "KS", "KY", "LA", "MA", "MD", "ME", "MI", "MN", "MO", "MS", "MT", "NC", "ND", "NE", "NH", "NJ", "NM", "NV", "NY", "OH", "OK", "OR", "PA", "RI", "SC", "SD", "TN", "TX", "UT", "VA", "VT", "WA", "WI", "WV", "WY"];
 
     if (!states.includes(stateName)) {
         const message = `Error: no data for state ${stateName}`;
